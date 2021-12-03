6955c922bf18fb1074784e1e90d8aa0c17e7d940
Samy <aurelien.demabre@etud.u-picardie.fr>
2021-12-03 11:39:50 +0100

    edit

---
+++ Oxo.c
@@ -0,0 +1,19 @@
+#include<stdio.h> 
+#include<stdlib.h>
+
+int impotOxo(int age; char sexe)
+
+int main()
+{
+    int age;
+    char sexe;
+    printf("Saisir un age\n")
+    scanf("%d",&age);
+    printf("Saisir un sexe (M,F,...)\n");
+    scanf("%c",&sexe);
+
+    if(sexe == 'M' && age >= 20 || sexe == 'F' && 18 < age && age < 35)
+        printf("Imposable\n");
+    else
+        printf("non imposable\n")
+}
