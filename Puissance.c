b94c8c07c13b46fc6ed1b8e692019c88e3159e25
Samy <aurelien.demabre@etud.u-picardie.fr>
2021-12-08 09:37:15 +0100

    sous prog

--- Puissance.c
+++ Puissance.c
@@ -1,18 +1,25 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int puissance(int nb, int puiss)
+{
+    int resultat = 1;
+
+    for(int i = 0; i < puiss; i++)
+        resultat = resultat * nb;
+
+    return resultat;
+}
 
 int main(){
-    int a, n, res = 1;
+    int a, n;
 
     printf("Saisir a\n");
     scanf("%d",&a);
     printf("Saisir n\n");
     scanf("%d",&n);
-
-    for(int i = 0; i < n; i++)
-        res = res * a;
-
-    printf("La puissance vaut %d\n", res);
+    printf("La puissance vaut %d\n", puissance(a,n));
     return 0;
 }
+
+// Ctrl + Shift + B to compile with GNU
