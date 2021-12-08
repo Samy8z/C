b02a17e76c830171b99c73a2b2662892dc81424d
Samy <aurelien.demabre@etud.u-picardie.fr>
2021-12-08 09:05:12 +0100

    create puissance.c

---
+++ Puissance.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(){
+    int a, n, res;
+
+    printf("Saisir a\n");
+    scanf("%d",&a);
+    printf("Saisir n\n");
+    scanf("%d",&n);
+
+    for(int i = 0; i < n; i++)
+        res = res * a;
+
+    printf("La puissance vaut %d\n", res);
+    return 0;
+}
