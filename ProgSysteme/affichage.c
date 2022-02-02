3cb3a3c0750d474e2ef400935fdbba2cdf3d50b3
Samy <aurelien.demabre@etud.u-picardie.fr>
2022-02-02 15:15:55 +0100

    Add "ProgSys"

---
+++ ProgSysteme/affichage.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+void affiche(float val)
+{
+printf("Résultat = %f\n", val);
+}
---
+++ ProgSysteme/calcul.c
@@ -0,0 +1,5 @@
+#include "moyenne.h"
+float total(float valeur, float chiffre)
+{
+return (valeur + chiffre / NBREVALEUR);
+}
