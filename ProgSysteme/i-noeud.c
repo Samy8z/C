fcc745b95c332dc98961c448472acd257ce142c4
Samy <samy@gmail.com>
2022-03-25 08:19:50 +0100

    add grp.h and pwd.h

--- ProgSysteme/i-noeud.c
+++ ProgSysteme/i-noeud.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
-#include <grp.h>
-#include <pwd.h>
+#include "grp.h"
+#include "pwd.h"
 #include <time.h>
 #include <unistd.h>
 int main(int argn, char *argv[])
